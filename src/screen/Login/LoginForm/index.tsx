32b66ff242f7bb3b8e04aba828d59c80b61a8db7
WillMaac <anderrwil@gmail.com>
2026-07-02 20:16:55 -0300

    feat: conclui layout da tela de login

--- src/screen/Login/LoginForm/index.tsx
+++ src/screen/Login/LoginForm/index.tsx
@@ -1,42 +1,44 @@
 import { AppButton } from '@/components/AppButton';
 import { AppInput } from '@/components/AppInput';
 import { useForm } from 'react-hook-form';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+
 export interface FormLoginParams {
   email: string;
   password: string;
 }
 
 export function LoginForm() {
-  const {
-    control,
-    handleSubmit,
-  } = useForm<FormLoginParams>();
+  const { control, handleSubmit } = useForm<FormLoginParams>();
 
-  return(<>
-  
-  <AppInput
-  control={control} 
-  name="email" 
-  label="EMAIL"
-  placeholder="mail@exemplo.br"
-  leftIconName="mail-outline"
-  />
+  return (
+    <>
+      <AppInput
+        control={control}
+        name="email"
+        label="EMAIL"
+        placeholder="mail@exemplo.br"
+        leftIconName="mail-outline"
+      />
 
-  <AppInput
-  control={control} 
-  name="password" 
-  label="SENHA"
-  placeholder="Sua senha"
-  leftIconName="lock-outline"
-  secureTextEntry
-  />
+      <AppInput
+        control={control}
+        name="password"
+        label="SENHA"
+        placeholder="Sua senha"
+        leftIconName="lock-outline"
+        secureTextEntry
+      />
 
-  <AppButton 
-  mode="fill"
-  iconName="arrow-forward"
-  
-  >Login</AppButton>
-  </>
-  )
+      <View className="flex-1 justify-between mt-8 mb-6 min-h-[250px]">
+        <AppButton iconName="arrow-forward">Login</AppButton>
+<View>
+  <Text className="mb-6 text-gray-300 text-base">Ainda não possui uma conta?</Text>
+        <AppButton iconName="arrow-forward" mode="outline">
+          Cadastrar
+        </AppButton>
+        </View>
+      </View>
+    </>
+  );
 }
